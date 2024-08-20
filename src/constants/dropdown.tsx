177137740f2080fe3e580575765528e5b6270285
Rahul Kumar <67744695+rahulkumar1241@users.noreply.github.com>
2024-08-20 18:45:22 +0530

    Update dropdown.tsx

--- src/constants/dropdown.tsx
+++ src/constants/dropdown.tsx
@@ -2,7 +2,7 @@ export const COUNTRY_CODE = [
     {
         value: "+91",
         label: "India (+91)"
-    }, 
+    },
     {
         value: "+977",
         label: "Nepal (+977)"
@@ -10,5 +10,69 @@ export const COUNTRY_CODE = [
     {
         value: "+880",
         label: "Bangladesh (+977)"
+    }
+]
+
+export const SIZE_DROPDOWN_WAIST_UP = [{
+    label: "S",
+    value: "S"
+},
+{
+    label: "M",
+    value: "M"
+},
+{
+    label: "L",
+    value: "L"
+},
+{
+    label: "XL",
+    value: "XL"
+},
+{
+    label: "XXL",
+    value: "XXL"
+}
+]
+
+export const SIZE_DROPDOWN_WAIST_DOWN = [
+    {
+        label: "28 inch",
+        value: "28 inch"
+    },
+    {
+        label: "30 inch",
+        value: "30 inch"
+    },
+    {
+        label: "32 inch",
+        value: "32 inch"
+    },
+    {
+        label: "34 inch",
+        value: "34 inch"
+    },
+    {
+        label: "36 inch",
+        value: "36 inch"
+    }
+]
+
+export const SIZE_DROPDOWN_SHOES = [
+    {
+        label: "7 UK",
+        value: "7 UK"
+    },
+    {
+        label: "8 UK",
+        value: "8 UK"
+    },
+    {
+        label: "9 UK",
+        value: "9 UK"
+    },
+    {
+        label: "10 UK",
+        value: "10 UK"
     }
 ]
