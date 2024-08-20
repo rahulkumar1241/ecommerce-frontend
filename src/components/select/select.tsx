b4c1626d7337c949e3a8b6d3940564232e794446
Rahul Kumar <67744695+rahulkumar1241@users.noreply.github.com>
2024-08-20 18:30:21 +0530

    Update select.tsx

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -4,12 +4,12 @@ import "./select.scss";
 
 const MySelect = (props: any) => {
 
-    const { menuItems, value, onChange, error, errormessage, label, required,style} = props;
+    const { menuItems, value, onChange, error, errormessage, label, required,style,size} = props;
 
     return <React.Fragment>
         <div className={!error ? "my-select" : "my-select-error"} style={{...style}}>
             <label>{label || "No Label"}<span className="text-danger">{required ? '*' : ''}</span></label>
-            <select value={value} onChange={onChange} className="form-select" aria-label="Default select example">
+            <select value={value} onChange={onChange} className={`${size ?'form-select-'+size:'form-select'}`} aria-label="Default select example">
                 <option value=''>Select an option</option>
                 {menuItems.map((data: any, index: any) => {
                     return <option value={data.value}>{data.label}</option>
@@ -20,5 +20,4 @@ const MySelect = (props: any) => {
     </React.Fragment>
 }
 
-export default MySelect
-
+export default MySelect;
